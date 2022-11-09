808408cc33c2f24dada860d75c80d8a305ee1d7f
marisalen <mlenarduzzi1@my.bcit.ca>
2022-11-08 16:16:27 -0800

    new

--- firebase/firebase.config.js
+++ firebase/firebase.config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDy_CohXC_FBzKD7lFrsJMPi3HF7aWxaqE",
-  authDomain: "wednesday-project-5728c.firebaseapp.com",
-  projectId: "wednesday-project-5728c",
-  storageBucket: "wednesday-project-5728c.appspot.com",
-  messagingSenderId: "348266683752",
-  appId: "1:348266683752:web:0879151dd5dd73aade8add"
+    apiKey: process.env.NEXT_PUBLIC_apiKey,
+    authDomain: process.env.NEXT_PUBLIC_authDomain,
+    projectId: process.env.NEXT_PUBLIC_projectId,
+    storageBucket: process.env.NEXT_PUBLIC_storageBucket,
+    messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
+    appId: process.env.NEXT_PUBLIC_appId
 };
 
 const app = initializeApp(firebaseConfig);
